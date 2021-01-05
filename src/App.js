aeee0e89bcd12b024207d306a63cafecadf605ff
Shadee Merhi <shadmerhi@gmail.com>
2021-01-04 21:24:22 -0700

    layout completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import { useApplicationData } from './hooks/useApplicationData';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Search from './components/Search';
+import Results from './components/Results';
+import Nominations from './components/Nominations';
+
+
+const useStyles = makeStyles((theme) => ({
+
+  root: {
+    border: '1px solid red'
+  }
+}));
 
 export default function App() {
+
+  const classes = useStyles();
 
   const {
     searchResults
@@ -10,8 +25,16 @@ export default function App() {
 
 
   return (
-    <div className="App">
-      <h1>Shopify Challenge!</h1>
-    </div>
+    <Grid container className={classes.root}>
+      <Grid item xs={12}>
+        <Search />
+      </Grid>
+      <Grid item xs={6}>
+        <Results />
+      </Grid>
+      <Grid item xs={6}>
+        <Nominations />
+      </Grid>
+    </Grid>
   );
 }
